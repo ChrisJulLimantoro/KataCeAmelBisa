07241ee4c1d4cebab75aa2349651b1444ac3c6ab
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-07-08 15:13:07 +0700

    Initial Commit

---
+++ KataCeAmelBisa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KataCeAmelBisa
+//
+//  Created by Christopher Julius on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KataCeAmelBisa/KataCeAmelBisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KataCeAmelBisaApp.swift
+//  KataCeAmelBisa
+//
+//  Created by Christopher Julius on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct KataCeAmelBisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
